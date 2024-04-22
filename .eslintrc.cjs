c13de6257becd3f8b1ff0a508f4bc19937b7967a
doctork4 <doctor.k1994@gmail.com>
2024-04-22 23:36:42 +0900

    feat: 프로젝트 세팅

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,33 @@
 /* eslint-env node */
-require('@rushstack/eslint-patch/modern-module-resolution')
+require('@rushstack/eslint-patch/modern-module-resolution');
 
 module.exports = {
   root: true,
-  'extends': [
+  extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
-    '@vue/eslint-config-prettier/skip-formatting'
+    '@vue/eslint-config-prettier',
   ],
+  env: {
+    'vue/setup-compiler-macros': true,
+  },
   parserOptions: {
-    ecmaVersion: 'latest'
-  }
-}
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        semi: true,
+        tabWidth: 2,
+        trailingComma: 'all',
+        printWidth: 80,
+        bracketSpacing: true,
+        arrowParens: 'avoid',
+        endOfLine: 'auto', // 한줄 추가
+      },
+    ],
+  },
+};
