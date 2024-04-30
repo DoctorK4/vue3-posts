7016f0974a8225863df99f8a836480cb99e0d69f
doctork4 <doctor.k1994@gmail.com>
2024-04-30 22:48:57 +0900

    feat: pagination 구현

--- src/views/posts/PostListView.vue
+++ src/views/posts/PostListView.vue
@@ -2,6 +2,21 @@
   <div>
     <h2>게시글 목록</h2>
     <hr class="my-4" />
+    <form @submit.prevent>
+      <div class="row g-3">
+        <div class="col">
+          <input type="text" class="form-control" v-model="params.title_like" />
+        </div>
+      </div>
+    </form>
+    <hr class="my-4" />
+    <div class="col-3">
+      <select class="form-select" v-model="params._limit">
+        <option value="3">3개씩 보기</option>
+        <option value="6">6개씩 보기</option>
+        <option value="9">9개씩 보기</option>
+      </select>
+    </div>
     <div class="row g-3">
       <div v-for="post in posts" class="col-4" :key="post.id">
         <PostItem
@@ -11,6 +26,45 @@
           @click="goPage(post.id)"
         />
       </div>
+      <nav aria-label="Page navigation example">
+        <ul class="pagination justify-content-center">
+          <li class="page-item">
+            <a
+              @click.prevent="params._page--"
+              class="page-link"
+              :class="{ disabled: params._page === 1 }"
+              href="#"
+              aria-label="Previous"
+            >
+              <span aria-hidden="true">&laquo;</span>
+            </a>
+          </li>
+          <li
+            v-for="page in pageCount"
+            :key="page"
+            class="page-item"
+            :class="{ active: params._page === page }"
+          >
+            <a
+              class="page-link"
+              href="#"
+              @click.prevent="params._page = page"
+              >{{ page }}</a
+            >
+          </li>
+          <li class="page-item">
+            <a
+              class="page-link"
+              :class="{ disabled: params._page === pageCount }"
+              href="#"
+              aria-label="Next"
+              @click.prevent="params._page++"
+            >
+              <span aria-hidden="true">&raquo;</span>
+            </a>
+          </li>
+        </ul>
+      </nav>
       <AppCard>
         <PostDetailView :id="2"></PostDetailView>
       </AppCard>
@@ -19,7 +73,7 @@
 </template>
 
 <script setup>
-import { ref } from 'vue';
+import { computed, ref, watchEffect } from 'vue';
 import { useRouter } from 'vue-router';
 import PostItem from '@/components/posts/PostItem.vue';
 import { getPosts } from '@/api/posts';
@@ -28,6 +82,20 @@ import AppCard from '@/components/AppCard.vue';
 
 const router = useRouter();
 const posts = ref([]);
+
+const params = ref({
+  _sort: 'createdAt',
+  _order: 'desc',
+  _page: 1,
+  _limit: 3,
+  title_like: '',
+});
+
+// pagination
+const totalCount = ref(0);
+const pageCount = computed(() =>
+  Math.ceil(totalCount.value / params.value._limit),
+);
 
 const goPage = id => {
   // router.push(`/posts/${id}`);
@@ -39,14 +107,15 @@ const goPage = id => {
 
 const fetchPosts = async () => {
   try {
-    const { data } = await getPosts();
+    const { data, headers } = await getPosts(params.value);
     posts.value = data;
+    totalCount.value = headers['x-total-count'];
   } catch (error) {
     console.error(error);
   }
 };
 
-fetchPosts();
+watchEffect(fetchPosts);
 </script>
 
 <style lang="scss" scoped></style>
